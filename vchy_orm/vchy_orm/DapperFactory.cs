81cb0e23af9899ec850575854de40fbca726f18b
vchy <2558575863@qq.com>
2019-03-21 15:04:49 +0800

    add orm project

---
+++ vchy_orm/vchy_orm/DapperFactory.cs
@@ -0,0 +1,19 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+
+
+namespace vchy_orm
+{
+    public class DapperFactory
+    {
+        private string _conn;
+
+        public DapperFactory()
+        {
+            
+        }
+
+        public IDbConnection Connection => new SqlConnection();
+
+    }
+}
